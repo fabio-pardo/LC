9d48dbe61f445b6f7ec9a560cf78e2cb9855c321
Fabio <fpardo1023@gmail.com>
2020-07-10 18:02:43 -0400

    questions route now able to post to MongoDB

--- backend/routes/questions.js
+++ backend/routes/questions.js
@@ -1,31 +1,77 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var mongo = require('mongodb');
+var mongo = require("mongodb");
 var MongoClient = mongo.MongoClient;
 
-var domain = '127.0.0.1';
+var domain = "127.0.0.1";
 var port = 27017;
 var dbName = "test";
 var destinationNode = "mongodb://" + domain + ":" + port;
 
-router.get('/', function(req, res, next) {
-  var documents = [];
-  MongoClient.connect(destinationNode, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  }, function(err, client) {
-    const db = client.db(dbName);
-    const collection = db.collection('questions');
+router.use(function (req, res, next) {
+  console.log(req.url, "@", Date.now());
+  next();
+});
 
-    var cursor = collection.find().sort({"_id": 1})
-    cursor.forEach(function(doc, err) {
-      documents.push(doc);
-    }, function() {
-      client.close();
-      console.log(documents);
-      res.render('questions', {name: documents})
-    })
+router.route("/").get((req, res, next) => {
+  var documents = [];
+  MongoClient.connect(
+    destinationNode,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    },
+    function (err, client) {
+      // Retrieving the collection questions from MongoDB test
+      var db = client.db(dbName);
+      var collection = db.collection("questions");
+
+      var cursor = collection.find().sort({ _id: 1 });
+      cursor.forEach(
+        function (doc, err) {
+          documents.push(doc);
+        },
+        function () {
+          client.close();
+          console.log(documents);
+          res.render("questions", { name: documents });
+        }
+      );
+    }
+  );
+});
+
+router
+  .route("/:questionid")
+  .get((req, res) => {
+    res.send("hi get /questions/" + req.params.questionid);
   })
-})
+  .post((req, res) => {
+    MongoClient.connect(
+      destinationNode,
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      },
+      function (err, client) {
+        // Retrieving the collection questions from MongoDB test
+        var db = client.db(dbName);
+        var collection = db.collection("questions");
+
+        // document to be inserted
+        // test
+        var doc = { _id: 5, name: "Test", difficulty: "Easy", passed: "true" };
+
+        // inserting document to 'questions collection' using insertOne
+        var cursor = collection.insertOne(doc, function (err, res) {
+          if (err) {
+            console.log(err)
+          };
+          console.log("New Question Inserted");
+        });
+      }
+    );
+    res.send("hi put /questions/" + req.params.questionid);
+  });
 
 module.exports = router;
