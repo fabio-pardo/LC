874b08ba7e566349c87b9db57bae871db1133dc0
Fabio <fpardo1023@gmail.com>
2020-07-15 18:09:39 -0400

    /questions/:question# now returns information on that question. POST question# now upserts if not already in the collection. TODO: POST any question functionality

--- backend/routes/questions.js
+++ backend/routes/questions.js
@@ -44,7 +44,33 @@ router.route("/").get((req, res, next) => {
 router
   .route("/:questionid")
   .get((req, res) => {
-    res.send("hi get /questions/" + req.params.questionid);
+    // res.send("GET question: " + req.params.questionid);
+
+    var documents = [];
+    MongoClient.connect(
+      destinationNode,
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      },
+      function (err, client) {
+        // Retrieving the collection questions from MongoDB test
+        var db = client.db(dbName);
+        var collection = db.collection("questions");
+
+        var cursor = collection.find({ _id: parseInt(req.params.questionid) });
+        cursor.forEach(
+          function (doc, err) {
+            documents.push(doc);
+          },
+          function () {
+            client.close();
+            console.log(documents);
+            res.render("questions", { name: documents });
+          }
+        );
+      }
+    );
   })
   .post((req, res) => {
     MongoClient.connect(
@@ -60,15 +86,28 @@ router
 
         // document to be inserted
         // test
-        var doc = { _id: 5, name: "Test", difficulty: "Easy", passed: "true" };
+        var doc = {
+          _id: 5,
+          name: "Test Five",
+          difficulty: "Easy",
+          passed: "true",
+        };
 
         // inserting document to 'questions collection' using insertOne
-        var cursor = collection.insertOne(doc, function (err, res) {
-          if (err) {
-            console.log(err)
-          };
-          console.log("New Question Inserted");
-        });
+        // var cursor = collection.insertOne(doc, function (err, res) {
+        var cursor = collection.updateOne(
+          { _id: 5 },
+          { $set: doc },
+          { upsert: true },
+          function (err, res) {
+            if (err) {
+              console.log(err);
+            } else {
+              console.log("New Question Inserted/Updated");
+              console.log(JSON.stringify(doc));
+            }
+          }
+        );
       }
     );
     res.send("hi put /questions/" + req.params.questionid);
