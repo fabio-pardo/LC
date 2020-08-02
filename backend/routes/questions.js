1d6d82abf0fa5f8898885c316bf61c513b7ecf54
Fabio <fpardo1023@gmail.com>
2020-08-02 19:06:53 -0400

    TODO:search quest bank and send compared

--- backend/routes/questions.js
+++ backend/routes/questions.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var router = express.Router();
 var mongo = require("mongodb");
+const { get } = require(".");
 var MongoClient = mongo.MongoClient;
 
 var domain = "127.0.0.1";
@@ -9,14 +10,13 @@ var dbName = "test";
 var destinationNode = "mongodb://" + domain + ":" + port;
 
 router.use(function (req, res, next) {
-  console.log(req.url, "@", Date.now());
+  console.log("/questions" + req.url, "@", Date.now());
   next();
 });
 
 router
   .route("/")
   .get((req, res) => {
-    var questions = [];
     MongoClient.connect(
       destinationNode,
       {
@@ -26,23 +26,23 @@ router
       function (err, client) {
         // Retrieving the collection questions from MongoDB test
         var db = client.db(dbName);
-        var myquestions = db.collection("questions");
+        var questions = db.collection("questions");
         var questionBank = db.collection("questionBank");
-        var cursor = myquestions.find().sort({ _id: 1 }, function (err) {
-          if (err) {
-            console.log(err);
-          }
-        });
-        cursor.forEach(
-          (doc) => {
-            questions.push(doc);
-          },
-          function () {
-            client.close();
-            console.log("Questions sent");
-            res.send(questions);
-          }
-        );
+
+        var questionList = [];
+        questions
+          .find()
+          .sort({ _id: 1 })
+          .forEach((doc) => {
+            questionBank.findOne({ number: doc._id }, function (error, result) {
+              if (error) {
+                console.log(error);
+              }
+              questionList.push(result);
+            });
+          });
+
+        res.send("hi");
       }
     );
   })
@@ -82,9 +82,7 @@ router
     );
   });
 
-router.route("/:questionid").get((req, res) => {
-  // res.send("GET question: " + req.params.questionid);
-
+router.route("/attempted/:questionid").get((req, res) => {
   MongoClient.connect(
     destinationNode,
     {
@@ -97,6 +95,53 @@ router.route("/:questionid").get((req, res) => {
       var collection = db.collection("questions");
 
       collection.findOne({ _id: Number(req.params.questionid) }, function (
+        err,
+        result
+      ) {
+        if (err) {
+          console.log(err);
+        } else {
+          // send the question back to client
+          res.send(result);
+        }
+        client.close();
+      });
+    }
+  );
+});
+
+router.route("/questionBank/").get((req, res) => {
+  MongoClient.connect(
+    destinationNode,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    },
+    function (err, client) {
+      // Retrieving the collection questions from MongoDB test
+      var db = client.db(dbName);
+      var myquestions = db.collection("questionBank");
+      myquestions.find({}).toArray(function (err, docs) {
+        if (err) return res.status(500).send({ error: err });
+        res.send(docs);
+      });
+    }
+  );
+});
+
+router.route("/questionBank/:questionid").get((req, res) => {
+  MongoClient.connect(
+    destinationNode,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    },
+    function (err, client) {
+      // Retrieving the collection questions from MongoDB test
+      var db = client.db(dbName);
+      var collection = db.collection("questionBank");
+
+      collection.findOne({ number: Number(req.params.questionid) }, function (
         err,
         result
       ) {
