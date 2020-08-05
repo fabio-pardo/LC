3fd0dc43e622d1a7c768a8598d4bf02dd9d8efb3
Fabio <fpardo1023@gmail.com>
2020-08-05 14:00:56 -0400

    gitignore

--- backend/secrets.js
+++ backend/secrets.js
@@ -1,6 +1,7 @@
-const mongodb = "";
+const mongodb =
+  "mongodb+srv://fabio:Stewie1023.@cluster0.9fk7t.mongodb.net/leetcode?retryWrites=true&w=majority";
 
-const dbName = "";
+const dbName = "leetcode";
 
 //var domain = "127.0.0.1";
 //var port = 27017;
