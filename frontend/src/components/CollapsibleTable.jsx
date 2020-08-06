85f0639e82f3f82f036015e53c181b7a369a31ac
Fabio <fpardo1023@gmail.com>
2020-08-06 17:34:30 -0400

    Pagination functioning. TODO: Sorting by #, Difficulty, Status, and (maybe) Last Attempted

--- frontend/src/components/CollapsibleTable.jsx
+++ frontend/src/components/CollapsibleTable.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import PropTypes from "prop-types";
 import "../styles/CollapsibleTable.css";
 import TableContainer from "@material-ui/core/TableContainer";
 import Table from "@material-ui/core/Table";
@@ -6,8 +7,15 @@ import TableHead from "@material-ui/core/TableHead";
 import TableRow from "@material-ui/core/TableRow";
 import TableCell from "@material-ui/core/TableCell";
 import TableBody from "@material-ui/core/TableBody";
+import TableFooter from "@material-ui/core/TableFooter";
+import TablePagination from "@material-ui/core/TablePagination";
 import Paper from "@material-ui/core/Paper";
-import { withStyles } from "@material-ui/core/styles";
+import { withStyles, makeStyles, useTheme } from "@material-ui/core/styles";
+import IconButton from "@material-ui/core/IconButton";
+import FirstPageIcon from "@material-ui/icons/FirstPage";
+import KeyboardArrowLeft from "@material-ui/icons/KeyboardArrowLeft";
+import KeyboardArrowRight from "@material-ui/icons/KeyboardArrowRight";
+import LastPageIcon from "@material-ui/icons/LastPage";
 
 const StyledTableCell = withStyles((theme) => ({
   head: {
@@ -28,8 +36,94 @@ const StyledTableRow = withStyles((theme) => ({
   },
 }))(TableRow);
 
+const useStyles1 = makeStyles((theme) => ({
+  root: {
+    flexShrink: 0,
+    marginLeft: theme.spacing(2.5),
+  },
+}));
+
+function TablePaginationActions(props) {
+  const classes = useStyles1();
+  const theme = useTheme();
+  const { count, page, rowsPerPage, onChangePage } = props;
+  const handleFirstPageButtonClick = (event) => {
+    onChangePage(event, 0);
+  };
+
+  const handleBackButtonClick = (event) => {
+    onChangePage(event, page - 1);
+  };
+
+  const handleNextButtonClick = (event) => {
+    onChangePage(event, page + 1);
+  };
+
+  const handleLastPageButtonClick = (event) => {
+    onChangePage(event, Math.max(0, Math.ceil(count / rowsPerPage) - 1));
+  };
+  return (
+    <div className={classes.root}>
+      <IconButton
+        onClick={handleFirstPageButtonClick}
+        disabled={page === 0}
+        aria-label="first page"
+      >
+        {theme.direction === "rtl" ? <LastPageIcon /> : <FirstPageIcon />}
+      </IconButton>
+      <IconButton
+        onClick={handleBackButtonClick}
+        disabled={page === 0}
+        aria-label="previous page"
+      >
+        {theme.direction === "rtl" ? (
+          <KeyboardArrowRight />
+        ) : (
+          <KeyboardArrowLeft />
+        )}
+      </IconButton>
+      <IconButton
+        onClick={handleNextButtonClick}
+        disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+        aria-label="next page"
+      >
+        {theme.direction === "rtl" ? (
+          <KeyboardArrowLeft />
+        ) : (
+          <KeyboardArrowRight />
+        )}
+      </IconButton>
+      <IconButton
+        onClick={handleLastPageButtonClick}
+        disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+        aria-label="last page"
+      >
+        {theme.direction === "rtl" ? <FirstPageIcon /> : <LastPageIcon />}
+      </IconButton>
+    </div>
+  );
+}
+
+TablePaginationActions.propTypes = {
+  count: PropTypes.number.isRequired,
+  onChangePage: PropTypes.func.isRequired,
+  page: PropTypes.number.isRequired,
+  rowsPerPage: PropTypes.number.isRequired,
+};
+
 function CollapsibleTable() {
   const [questions, setQuestions] = useState([]);
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(5);
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
 
   useEffect(() => {
     fetch("/questions", { method: "GET" })
@@ -54,43 +148,67 @@ function CollapsibleTable() {
           </TableRow>
         </TableHead>
         <TableBody>
-          {questions.length > 0 ? (
-            questions.map((question) => (
-              <StyledTableRow key={question.number}>
-                <TableCell align="center">{question.number}</TableCell>
-                <TableCell align="center">
-                  <a href={question.questionInfo[0].titleInfo[1]}>
-                    {question.questionInfo[0].titleInfo[0]}
-                  </a>
+          {(rowsPerPage > 0
+            ? questions.slice(
+                page * rowsPerPage,
+                page * rowsPerPage + rowsPerPage
+              )
+            : questions
+          ).map((question) => (
+            <StyledTableRow key={question.number}>
+              <TableCell align="center">{question.number}</TableCell>
+              <TableCell align="center">
+                <a href={question.questionInfo[0].titleInfo[1]}>
+                  {question.questionInfo[0].titleInfo[0]}
+                </a>
+              </TableCell>
+              <TableCell align="center">
+                {question.questionInfo[0].difficulty}
+              </TableCell>
+              {question.passed ? (
+                <TableCell
+                  style={{ backgroundColor: "#4caf50", color: "white" }}
+                  align="center"
+                >
+                  Passed
                 </TableCell>
-                <TableCell align="center">
-                  {question.questionInfo[0].difficulty}
+              ) : (
+                <TableCell
+                  style={{ backgroundColor: "#f44336", color: "white" }}
+                  align="center"
+                >
+                  Failed
                 </TableCell>
-                {question.passed ? (
-                  <TableCell
-                    style={{ backgroundColor: "#4caf50", color: "white" }}
-                    align="center"
-                  >
-                    Passed
-                  </TableCell>
-                ) : (
-                  <TableCell
-                    style={{ backgroundColor: "#f44336", color: "white" }}
-                    align="center"
-                  >
-                    Failed
-                  </TableCell>
-                )}
-                <TableCell align="center">{question.date}</TableCell>
-                <TableCell align="center">
-                  <a href={question.questionInfo[0].solution}>Java</a>
-                </TableCell>
-              </StyledTableRow>
-            ))
-          ) : (
-            <TableRow></TableRow>
-          )}
+              )}
+              <TableCell align="center">{question.date}</TableCell>
+              <TableCell align="center">
+                <a href={question.questionInfo[0].solution}>Java</a>
+              </TableCell>
+            </StyledTableRow>
+          ))}
         </TableBody>
+        <TableFooter>
+          <TableRow>
+            <TablePagination
+              rowsPerPageOptions={[
+                5,
+                10,
+                25,
+                { label: "All", value: questions.length },
+              ]}
+              count={questions.length}
+              rowsPerPage={rowsPerPage}
+              page={page}
+              SelectProps={{
+                inputProps: { "aria-label": "rows per page" },
+                native: true,
+              }}
+              onChangePage={handleChangePage}
+              onChangeRowsPerPage={handleChangeRowsPerPage}
+              ActionsComponent={TablePaginationActions}
+            />
+          </TableRow>
+        </TableFooter>
       </Table>
     </TableContainer>
   );
