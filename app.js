e6d7139738d1d0ba4960f8e9523fa12ff5960d66
Fabio <fpardo1023@gmail.com>
2020-08-12 11:06:21 -0400

    LCAPP working with heroku on firefox but not chrome. Weird

--- app.js
+++ app.js
@@ -24,10 +24,13 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/", indexRouter);
+app.use("/", express.static(path.join(__dirname, "/client/build")));
 app.use("/users", usersRouter);
 app.use("/questions", questionsRouter);
 
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "/client/build", "index.html"));
+});
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
@@ -48,8 +51,8 @@ hbs.registerHelper("json", function (context) {
   return JSON.stringify(context);
 });
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-}
+//if (process.env.NODE_ENV === "production") {
+//app.use(express.static("client/build"));
+//}
 
 module.exports = app;
